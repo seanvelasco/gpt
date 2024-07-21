1debe946a7878082aad65d6332707492df4830e3
Sean Velasco <sean@sean.app>
2024-07-21 09:10:03 +0800

    Add jupyter notebook

--- gpt.py
+++ gpt.py
@@ -19,8 +19,22 @@ encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: "".join([itos[i] for i in l])
 
 # text represented as torch.Tensor
-t = tensor(encode(text), dtype=torch.long)
-print(t.shape)
-print(t.dtype)
+data = tensor(encode(text), dtype=torch.long)
 
 # split up the data into train and validation sets
+# 90% of the data will be the training data
+# remaining 10% of the data will be validation data
+n = int(.9 * len(data)) # 
+train_data, val_data = data[:n], data[n:] 
+
+batch_size = 4 # number of independent sequences to be processed in parallel
+block_size = 8 # maximum context length to make predictions
+
+# We train them chunks at a time
+
+x = train_data[:block_size]
+y = train_data[1:block_size + 1]
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
+    print(f"when input is {context} the target is {target}")
